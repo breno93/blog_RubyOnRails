7d4018fc963218355ca971a61d8baf47639254b3
Breno93 <brenoalmeidasantos1@gmail.com>
2025-02-25 18:18:51 -0300

    create db comments and rich text; added /views/comments/...; render comments in /views/posts/show.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :posts
+
+  resources :posts do # /posts/1/comments/2
+    resources :comments
+  end
+
+
   get "up" => "rails/health#show", as: :rails_health_check
   get "home", controller: "home", action: "index", as: "home"
   # get "estudante", to: "estudante#index", as: "estudantes"
